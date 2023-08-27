ec5a4571866b9b35b0fb2e76f1426f8bdbaa4ac2
thusy <12thusy1999@gmail.com>
2023-08-27 13:04:33 +0530

    swagger not enabled

--- src/main/java/com/example/test/security/WebSecurityConfig.java
+++ src/main/java/com/example/test/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.test.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +24,8 @@ import com.example.test.security.jwt.AuthTokenFilter;
 import com.example.test.security.services.UserDetailsServiceImpl;
 
 @Configuration
+@EnableWebSecurity
+@Order(1) // Set an appropriate order
 @EnableMethodSecurity
 // (securedEnabled = true,
 // jsr250Enabled = true,
@@ -68,6 +72,17 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+//  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+            .requestMatchers("/v2/api-docs", "/swagger-ui.html", "/swagger-ui/**", "/webjars/**", "/swagger-resources/**")
+            .permitAll() // Allow access to Swagger documentation
+            .anyRequest().authenticated();
+//            .and()
+//            .csrf().disable(); // You might need to disable CSRF if it's causing issues
+  }
+
 
 //  @Override
 //  protected void configure(HttpSecurity http) throws Exception {
